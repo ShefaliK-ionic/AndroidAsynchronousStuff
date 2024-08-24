bb2a625a709a98145e73db3c3f9c8ce2a78859b9
Shefali Kanojiya <shefalismt@gmail.com>
2024-08-24 19:25:28 +0530

    coroutine scope

--- app/src/main/java/com/coroutine/MainActivity.kt
+++ app/src/main/java/com/coroutine/MainActivity.kt
@@ -32,7 +32,9 @@ class MainActivity : AppCompatActivity() {
 // supervisorScopeForExecuteOtherEvenExcptionForSibling()
 //        coroutineExceptionHandler()
 //        deferredObjectAsync()
-        couroutineScope()
+//        couroutineScope()
+
+
     }
 
     private fun couroutineScope() {
